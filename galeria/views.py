2547023839029724b58a8a8951d68d4359567656
David Lucas Espindola de Barros <david.barros@fps.edu.br>
2025-08-29 15:08:44 -0300

    aplicando as primeiras migrações

--- galeria/views.py
+++ galeria/views.py
@@ -5,9 +5,9 @@ from django.shortcuts import render
 def index(request):
 
     dados = {
-    1: {"nome": "Nebulosa de Carina", "legenda": "webbtelecope.org /NASA/ James Webb"},
-    2: {"nome": "Galáxia NGC 1079", "legenda": "nasa.org / NASA / Hubble"}
-}
+        1: {"nome": "Nebulosa de Carina", "legenda": "webbtelecope.org /NASA/ James Webb"},
+        2: {"nome": "Galáxia NGC 1079", "legenda": "nasa.org / NASA / Hubble"}
+    }
 
     return render(request, 'galeria/index.html', {"cards": dados})
 
